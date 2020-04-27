bb88f8aceae123f7db5da06d8802a07b4acccb94
Sandeep Srinivasa <sss@lambdacurry.com>
2020-04-28 03:10:53 +0530

    Card swipe test fix (#1)
    * fixed tests to create User properly and pass amount correctly
    
    * updated black formatting

--- src/rush/utils.py
+++ src/rush/utils.py
@@ -1,27 +1,92 @@
 import sqlalchemy
 
-from rush.models import LedgerTriggerEvent, LedgerEntry, get_current_ist_time, BookAccount,get_or_create
+from rush.models import (
+    LedgerTriggerEvent,
+    LedgerEntry,
+    get_current_ist_time,
+    BookAccount,
+    get_or_create,
+    User,
+)
 
 
-def insert_payments(
-    session: sqlalchemy.orm.session.Session, event_name: str, extra_details: dict
+def insert_card_swipe(
+    session: sqlalchemy.orm.session.Session,
+    user: User,
+    event_name: str,
+    extra_details: dict,
+    amount: int,
 ) -> None:
-    u = LedgerTriggerEvent(performed_by=123, name=event_name, extra_details=extra_details)
-    session.add(u)
+    lt = LedgerTriggerEvent(performed_by=user.id, name=event_name, extra_details=extra_details)
+    session.add(lt)
     session.flush()
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=100,book_type='dmi_pool_account',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=100,book_type='dmi_limit_used',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
-    session.add(le)
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=100,
+        book_type="dmi_pool_account",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=100,
+        book_type="dmi_limit_used",
+        account_type="asset",
+    )
+    le1 = LedgerEntry(
+        event_id=lt.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=amount,
+        business_date=get_current_ist_time(),
+    )
+    session.add(le1)
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_card_balance',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='unbilled_transactions',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
-    session.add(le)
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=user.id,
+        book_type="user_card_balance",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=user.id,
+        book_type="unbilled_transactions",
+        account_type="asset",
+    )
+    le2 = LedgerEntry(
+        event_id=lt.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=amount,
+        business_date=get_current_ist_time(),
+    )
+    session.add(le2)
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_marvin_limit',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_marvin_limit_used',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
-    session.add(le)
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=user.id,
+        book_type="user_marvin_limit",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=user.id,
+        book_type="user_marvin_limit_used",
+        account_type="asset",
+    )
+    le3 = LedgerEntry(
+        event_id=lt.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=amount,
+        business_date=get_current_ist_time(),
+    )
+    session.add(le3)
     session.commit()
