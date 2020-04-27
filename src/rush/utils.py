a90dfd0d3b71a658932ab28b23ece7174abdcdd8
Tony Benoy <me@tonybenoy.com>
2020-04-28 01:25:17 +0530

    changes

--- src/rush/utils.py
+++ src/rush/utils.py
@@ -1,6 +1,12 @@
 import sqlalchemy
 
-from rush.models import LedgerTriggerEvent, LedgerEntry, get_current_ist_time, BookAccount,get_or_create
+from rush.models import (
+    LedgerTriggerEvent,
+    LedgerEntry,
+    get_current_ist_time,
+    BookAccount,
+    get_or_create,
+)
 
 
 def insert_payments(
@@ -10,18 +16,72 @@ def insert_payments(
     session.add(u)
     session.flush()
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=100,book_type='dmi_pool_account',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=100,book_type='dmi_limit_used',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=100,
+        book_type="dmi_pool_account",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=100,
+        book_type="dmi_limit_used",
+        account_type="asset",
+    )
+    le = LedgerEntry(
+        event_id=u.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=100,
+        business_date=get_current_ist_time(),
+    )
     session.add(le)
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_card_balance',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='unbilled_transactions',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=200,
+        book_type="user_card_balance",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=200,
+        book_type="unbilled_transactions",
+        account_type="asset",
+    )
+    le = LedgerEntry(
+        event_id=u.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=100,
+        business_date=get_current_ist_time(),
+    )
     session.add(le)
 
-    from_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_marvin_limit',account_type='liability')
-    to_account=get_or_create(session=session,model=BookAccount,identifier=200,book_type='user_marvin_limit_used',account_type='asset')
-    le = LedgerEntry( event_id=u.id, from_book_account=from_account.id,to_book_account=to_account.id,amount=100,business_date=get_current_ist_time())
+    from_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=200,
+        book_type="user_marvin_limit",
+        account_type="liability",
+    )
+    to_account = get_or_create(
+        session=session,
+        model=BookAccount,
+        identifier=200,
+        book_type="user_marvin_limit_used",
+        account_type="asset",
+    )
+    le = LedgerEntry(
+        event_id=u.id,
+        from_book_account=from_account.id,
+        to_book_account=to_account.id,
+        amount=100,
+        business_date=get_current_ist_time(),
+    )
     session.add(le)
     session.commit()
