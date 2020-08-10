6efa58e5f45faab8041a3b465845939f0ea72c5f
transhapHigsn <fet.prashantsingh@gmail.com>
2020-08-10 23:45:09 +0530

    starting health card product changes

---
+++ src/rush/card/health_card.py
@@ -0,0 +1,14 @@
+from rush.card.base_card import (
+    BaseBill,
+    BaseCard,
+)
+
+
+class HealthCard(BaseCard):
+    # todo: add implementation for health card.
+    pass
+
+
+class HealthBill(BaseBill):
+    # todo: add implementation for health card bills.
+    pass
