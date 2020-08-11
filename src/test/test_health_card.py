8c1c9b488e6cdf94f1c0144c2f8d6c753eb14bcb
transhapHigsn <fet.prashantsingh@gmail.com>
2020-08-11 20:23:39 +0530

    added create card swipe test for health card

--- src/test/test_health_card.py
+++ src/test/test_health_card.py
@@ -1,7 +1,20 @@
+from decimal import Decimal
+
 from pendulum import parse as parse_date  # type: ignore
 from sqlalchemy.orm import Session
 
-from rush.card import create_user_card
+from rush.card import (
+    create_user_card,
+)
+
+from rush.create_card_swipe import (
+    create_card_swipe,
+)
+
+from rush.ledger_utils import (
+    get_account_balance_from_str,
+)
+
 from rush.models import (
     CardKitNumbers,
     CardNames,
@@ -49,3 +62,38 @@ def test_create_health_card(session: Session) -> None:
     )
 
     assert uc.card_type == "health_card"
+
+
+def test_health_card_swipe(session: Session) -> None:
+    create_lenders(session)
+    card_db_updates(session)
+    create_user(session)
+    uc = create_user_card(
+        session=session,
+        user_id=3,
+        card_activation_date=parse_date("2020-08-11").date(),
+        card_type="health_card",
+        lender_id=62311,
+        kit_number="10000",
+    )
+
+    swipe1 = create_card_swipe(
+        session=session,
+        user_card=uc,
+        txn_time=parse_date("2020-08-11 18:30:10"),
+        amount=Decimal(700),
+        description="Amazon.com",
+    )
+    swipe1 = swipe1["data"]
+
+    _, unbilled_balance = get_account_balance_from_str(session, f"{swipe1.loan_id}/bill/unbilled/a")
+    assert unbilled_balance == 700
+
+    _, medical_limit_balance = get_account_balance_from_str(session, f"{uc.id}/card/health_limit/l")
+    assert medical_limit_balance == -700
+
+    _, non_medical_limit_balance = get_account_balance_from_str(session, f"{uc.id}/card/available_limit/l")
+    assert non_medical_limit_balance == 0
+
+    _, lender_payable = get_account_balance_from_str(session, f"{uc.id}/card/lender_payable/l")
+    assert lender_payable == 700
