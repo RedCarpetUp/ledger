d25fd22075c526fbb50faada28cfd301768b49c2
sarthak_saxena_72 <i.sarthak.saxena@redcarpetup.com>
2021-01-05 15:42:19 +0530

    new methods for rebel

--- src/rush/card/rebel_card.py
+++ src/rush/card/rebel_card.py
@@ -1,10 +1,18 @@
-from typing import Type
+from decimal import Decimal
+from operator import add
+from typing import (
+    List,
+    Type,
+)
+
+from sqlalchemy.sql.sqltypes import DateTime
 
 from rush.card.base_card import (
     B,
     BaseBill,
     BaseLoan,
 )
+from rush.card.transaction_loan import TransactionLoan
 
 
 class RebelBill(BaseBill):
@@ -13,5 +21,65 @@ class RebelBill(BaseBill):
 
 class RebelCard(BaseLoan):
     bill_class: Type[B] = RebelBill
+
+    def get_transaction_loans(self) -> List[TransactionLoan]:
+        return (
+            self.session.query(BaseLoan)
+            .filter(BaseLoan.product_type == "transaction_loan", BaseLoan.user_id == self.user_id)
+            .all()
+        )
+
+    def get_remaining_min(self, date_to_check_against: DateTime) -> Decimal:
+        txn_loans = self.get_transaction_loans()
+
+        txn_loans_remaining_min_sum = sum(
+            loan.get_remaining_min(date_to_check_against=date_to_check_against) for loan in txn_loans
+        )
+
+        return (
+            super().get_remaining_min(date_to_check_against=date_to_check_against)
+            + txn_loans_remaining_min_sum
+        )
+
+    def get_remaining_max(self, date_to_check_against: DateTime) -> Decimal:
+        txn_loans = self.get_transaction_loans()
+
+        txn_loans_remaining_max_sum = sum(
+            loan.get_remaining_max(date_to_check_against=date_to_check_against) for loan in txn_loans
+        )
+
+        return (
+            super().get_remaining_max(date_to_check_against=date_to_check_against)
+            + txn_loans_remaining_max_sum
+        )
+
+    def get_all_bills(
+        self,
+        are_generated: bool = False,
+        only_unpaid_bills: bool = False,
+        only_closed_bills: bool = False,
+    ) -> List[BaseBill]:
+        txn_loans = self.get_transaction_loans()
+
+        all_bills = []
+
+        for loan in txn_loans:
+            all_bills.extend(
+                loan.get_all_bills(
+                    are_generated=are_generated,
+                    only_unpaid_bills=only_unpaid_bills,
+                    only_closed_bills=only_closed_bills,
+                )
+            )
+
+        all_bills.extend(
+            super().get_all_bills(
+                are_generated=are_generated,
+                only_unpaid_bills=only_unpaid_bills,
+                only_closed_bills=only_closed_bills,
+            )
+        )
+
+        return all_bills
 
     __mapper_args__ = {"polymorphic_identity": "rebel"}
