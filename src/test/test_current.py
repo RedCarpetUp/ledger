9ebb734e62ae23c693a9157178d747b40e00c2a3
Nikhil Vangumalla <nikhil.vangumalla@redcarpetup.com>
2021-01-11 23:09:50 +0530

    Change gateway charges to lender level in testcase

--- src/test/test_current.py
+++ src/test/test_current.py
@@ -763,7 +763,7 @@ def _partial_payment_bill_1(session: Session) -> None:
 
     bill = unpaid_bills[0]
     _, gateway_expenses = get_account_balance_from_str(
-        session, book_string="12345/redcarpet/gateway_expenses/e"
+        session, book_string=f"{user_loan.lender_id}/lender/gateway_expenses/e"
     )
     assert gateway_expenses == 0.5
 
