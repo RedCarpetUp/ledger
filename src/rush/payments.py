b65b34692cb993890532ff0ac90d2875065b58d6
sahilmahajanred <i.sahil.mahajan@redcarpetup.com>
2021-03-06 19:33:29 +0530

    rounding issue fixed

--- src/rush/payments.py
+++ src/rush/payments.py
@@ -62,7 +62,7 @@ def payment_received(
     if payment_request_data.collection_by == "rc_lender_payment":
         write_off_loan(user_loan=user_loan, payment_request_data=payment_request_data)
         return
-
+    payment_for_loan = round(payment_for_loan, 2)
     event = LedgerTriggerEvent.new(
         session,
         name="payment_received",
@@ -247,14 +247,18 @@ def find_split_to_slide_in_loan(session: Session, user_loan: BaseLoan, total_amo
         for fee_type, fees in all_fees_by_type.items():
             total_fee_amount = sum(fee.remaining_fee_amount for fee in fees)
             total_amount_to_be_adjusted_in_fee = min(total_fee_amount, total_amount_to_slide)
-
+            fee_amount = 0
             for fee in fees:
                 # For non-bill aka loan-level fees
                 # Thus, they are not slid into bills and simply get added to the split info
                 # to be adjusted into the loan later
                 if fee.identifier == "loan":
                     amount_to_adjust = min(total_amount_to_be_adjusted_in_fee, fee.gross_amount)
-                    x = {"type": "fee", "fee": fee, "amount_to_adjust": amount_to_adjust}
+                    x = {
+                        "type": "fee",
+                        "fee": fee,
+                        "amount_to_adjust": amount_to_adjust,
+                    }
                     split_info.append(x)
                     continue
 
@@ -264,6 +268,10 @@ def find_split_to_slide_in_loan(session: Session, user_loan: BaseLoan, total_amo
                     fee.remaining_fee_amount / total_fee_amount,
                     total_amount_to_be_adjusted_in_fee,
                 )
+                fee_amount += amount_to_slide_based_on_ratio
+                difference = total_amount_to_slide - fee_amount
+                if difference < 0:
+                    amount_to_slide_based_on_ratio = amount_to_slide_based_on_ratio + difference
                 x = {
                     "type": "fee",
                     "bill": bill,
@@ -277,10 +285,16 @@ def find_split_to_slide_in_loan(session: Session, user_loan: BaseLoan, total_amo
     total_interest_amount = sum(bill.get_interest_due() for bill in unpaid_bills)
     if total_amount_to_slide > 0 and total_interest_amount > 0:
         total_amount_to_be_adjusted_in_interest = min(total_interest_amount, total_amount_to_slide)
+        interest_amount = 0
         for bill in unpaid_bills:
             amount_to_slide_based_on_ratio = mul(
-                bill.get_interest_due() / total_interest_amount, total_amount_to_be_adjusted_in_interest
+                bill.get_interest_due() / total_interest_amount,
+                total_amount_to_be_adjusted_in_interest,
             )
+            interest_amount += amount_to_slide_based_on_ratio
+            difference = total_amount_to_slide - interest_amount
+            if difference < 0:
+                amount_to_slide_based_on_ratio = amount_to_slide_based_on_ratio + difference
             if amount_to_slide_based_on_ratio > 0:  # will be 0 for 0 bill with late fee.
                 x = {
                     "type": "interest",
@@ -294,11 +308,17 @@ def find_split_to_slide_in_loan(session: Session, user_loan: BaseLoan, total_amo
     total_principal_amount = sum(bill.get_principal_due() for bill in unpaid_bills)
     if total_amount_to_slide > 0 and total_principal_amount > 0:
         total_amount_to_be_adjusted_in_principal = min(total_principal_amount, total_amount_to_slide)
+        principal_amount = 0
         for bill in unpaid_bills:
             amount_to_slide_based_on_ratio = mul(
                 bill.get_principal_due() / total_principal_amount,
                 total_amount_to_be_adjusted_in_principal,
             )
+            principal_amount += amount_to_slide_based_on_ratio
+            difference = total_amount_to_slide - principal_amount
+            if difference < 0:
+                amount_to_slide_based_on_ratio = amount_to_slide_based_on_ratio + difference
+
             if amount_to_slide_based_on_ratio > 0:
                 x = {
                     "type": "principal",
